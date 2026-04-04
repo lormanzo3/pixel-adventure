28b439978c9c8adeb4b8d761542d14601b0bf779
lormanzo3 <unframed3@gmail.com>
2026-04-04 11:07:21 +0200

    made hotspots for the gamecanvas

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -5,7 +5,27 @@ function GameCanvas({ scene, gameState, setGameState }: {
   gameState: GameState
   setGameState: (state: GameState) => void
 }) {
-    return <img src={scene.background} />
+    return (
+      <div style={{ position: "relative"}}>
+        <img src={scene.background} />
+        {scene.hotspots.map((hotspot) => (
+          <div
+            key={hotspot.id}
+            style={{
+              position: "absolute",
+              left: hotspot.area.x + "%",
+              top: hotspot.area.y + "%",
+              width: hotspot.area.width + "%",
+              height: hotspot.area.height + "%",
+              cursor: "pointer"
+            }}
+            onClick={() => {
+              setGameState(hotspot.action(gameState))
+            }}
+          />
+        ))}
+      </div>
+    )
   }
 
   export default GameCanvas
